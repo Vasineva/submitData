e9cd620adb61acbfda33198ec429ae93db29fe25
Katya <vasineva.1994@gmail.com>
2025-04-25 15:23:52 +1000

    Настроены сериализаторы для всех моделей с использованием serializers.ModelSerializer. Добавлена поддержка вложенных данных и словаря для уровней сложности.

--- PerevalBase/pereval/serializers.py
+++ PerevalBase/pereval/serializers.py
@@ -2,20 +2,33 @@ from .models import *
 from rest_framework import serializers
 
 
-class PerevalUserSerializer(serializers.HyperlinkedModelSerializer):
+class PerevalUserSerializer(serializers.ModelSerializer):
    class Meta:
-       pass
+       model = PerevalUser
+       fields = '__all__'
 
-
-class PerevalCoordsSerializer(serializers.HyperlinkedModelSerializer):
+class PerevalCoordsSerializer(serializers.ModelSerializer):
    class Meta:
-       pass
+       model = PerevalCoords
+       fields = '__all__'
 
-
-class PerevalAddedSerializer(serializers.HyperlinkedModelSerializer):
+class PerevalImageSerializer(serializers.ModelSerializer):
    class Meta:
-       pass
+       class Meta:
+           model = PerevalImage
+           fields = ['title', 'image_url']
 
-class PerevalImageSerializer(serializers.HyperlinkedModelSerializer):
+class PerevalAddedSerializer(serializers.ModelSerializer):
    class Meta:
-       pass
+       user = PerevalUserSerializer()
+       coords = PerevalCoordsSerializer()
+       images = PerevalImageSerializer(many=True, write_only=True) #many=True список изображений
+       # Уровни сложности по сезонам передаются как словарь
+       level = serializers.DictField(child=serializers.CharField(), write_only=True)
+
+       class Meta:
+           model = PerevalAdded
+           fields = [
+               'beauty_title', 'title', 'other_titles', 'connect', 'add_time',
+               'user', 'coords', 'level', 'images'
+           ]  # Поля, которые участвуют в сериализации
