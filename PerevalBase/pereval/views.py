32c22c58c7574b1ed94386d3b02be7bb79092331
Katya <vasineva.1994@gmail.com>
2025-04-26 09:15:54 +1000

    Создан класс SubmitData с методами для обработки POST-запроса Добавлена проверка данных на валидность с использованием сериализатора PerevalAddedSerializer.

--- PerevalBase/pereval/views.py
+++ PerevalBase/pereval/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PerevalAddedSerializer
+
+
+class SubmitData(APIView):
+    def post(self, request):
+        # для сериализатора
+        serializer = PerevalAddedSerializer(data=request.data)
+
+        # валидность данных
+        if serializer.is_valid():
+            return self.handle_valid_data(serializer)
+
+        # данные невалидны, возвращаем ошибку
+        return self.handle_invalid_data(serializer)
+
+    def handle_valid_data(self, serializer):
+        # сохранение данных
+        pass
+
+    def handle_invalid_data(self, serializer):
+        # обработка ошибкок
+        pass
