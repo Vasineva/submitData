a6b2a740762a6b1ec6ef1de46fef532a7d170fb0
Katya <vasineva.1994@gmail.com>
2025-04-26 10:35:18 +1000

    Завершена логика обработки в SubmitData: реализовано сохранение данных, добавлен обработчик ошибок при сохранении и валидации.

--- PerevalBase/pereval/views.py
+++ PerevalBase/pereval/views.py
@@ -7,20 +7,32 @@ from .serializers import PerevalAddedSerializer
 
 class SubmitData(APIView):
     def post(self, request):
-        # для сериализатора
         serializer = PerevalAddedSerializer(data=request.data)
-
-        # валидность данных
         if serializer.is_valid():
             return self.handle_valid_data(serializer)
-
-        # данные невалидны, возвращаем ошибку
         return self.handle_invalid_data(serializer)
 
     def handle_valid_data(self, serializer):
-        # сохранение данных
-        pass
+        try:
+            # Сохранение перевала, если данные валидны
+            pereval = serializer.save()
+            return Response({
+                'status': 200,
+                'message': None,
+                'id': pereval.id
+            }, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({
+                'status': 500,
+                'message': f'Ошибка при сохранении: {str(e)}',
+                'id': None
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def handle_invalid_data(self, serializer):
-        # обработка ошибкок
-        pass
+        # Возвращаем ошибку, если данные невалидны
+        return Response({
+            'status': 400,
+            'message': 'Ошибка валидации',
+            'errors': serializer.errors,
+            'id': None
+        }, status=status.HTTP_400_BAD_REQUEST)
