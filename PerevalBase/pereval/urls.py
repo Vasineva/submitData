8ccb186be19b8990171b420631ff8453d231b4c5
Katya <vasineva.1994@gmail.com>
2025-04-30 00:06:26 +1000

    - Реализован PATCH-метод для обновления записи с ID. - Запись может быть отредактирована только если её статус — 'new'. - Ограничение: нельзя изменять поля, связанные с пользователем. - При успешном обновлении возвращается `state: 1` и сообщение об успешном обновлении. - При попытке обновить запись с неправильным статусом возвращается `state: 0` и сообщение о причинах отказа.
    Задача завершена для эндпоинта PATCH /submitData/<id>.

--- PerevalBase/pereval/urls.py
+++ PerevalBase/pereval/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import SubmitData, PerevalDetail
+from .views import SubmitData, PerevalRetrieveUpdateView
 
 urlpatterns = [
-    path('submit_data/', SubmitData.as_view(), name='submit_data'),
-    path('submitData/<int:id>/', PerevalDetail.as_view(), name='submit_data_detail'),
+    path('submitData/', SubmitData.as_view(), name='submit_data'),
+    path('submitData/<int:id>/', PerevalRetrieveUpdateView.as_view(), name='submit_data_detail'),
 ]
