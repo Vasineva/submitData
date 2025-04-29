808e59f7812547cfdcf2a679fcc75021c0f95094
Katya <vasineva.1994@gmail.com>
2025-04-29 19:13:52 +1000

    Добавлен эндпоинт для получения информации о перевале по ID

--- PerevalBase/pereval/urls.py
+++ PerevalBase/pereval/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SubmitData
+from .views import SubmitData, PerevalDetail
 
 urlpatterns = [
     path('submit_data/', SubmitData.as_view(), name='submit_data'),
+    path('submitData/<int:id>/', PerevalDetail.as_view(), name='submit_data_detail'),
 ]
