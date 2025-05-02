10b7645e85233859948f76588226bc0632f630b7
Katya <vasineva.1994@gmail.com>
2025-05-02 17:36:38 +1000

    Настройка drf-yasg

--- PerevalBase/PerevalBase/settings.py
+++ PerevalBase/PerevalBase/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'pereval',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 SITE_ID = 1
--- PerevalBase/PerevalBase/urls.py
+++ PerevalBase/PerevalBase/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# Объект для Swagger документации
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FSTR API",
+        default_version="v1",
+        description="API для работы с горными перевалами",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),  # Без авторизации
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('pereval.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
